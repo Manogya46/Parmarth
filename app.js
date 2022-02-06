a67ee7cbc7d326fff1b63939a331901da8137663
manogya mishra <dev.techdock@gmail.com>
2022-02-06 14:18:17 +0530

    Initial commit

--- app.js
+++ app.js
@@ -173,6 +173,12 @@ app.post("/login", function(req,res){
     })
 });
 
-app.listen(3000,() =>{
+
+let port = process.env.PORT;
+if(port==null || port==""){
+    port = 3000;
+}
+
+app.listen(port,() =>{
     console.log("Server is up and running");
 })
